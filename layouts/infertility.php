81f27b4bc8c17facc2b0ed98cbab234e91df8895
gazala1234 <ustadgazal@gmail.com>
2024-10-14 15:06:15 +0530

    responsive departments

--- layouts/infertility.php
+++ layouts/infertility.php
@@ -6,20 +6,6 @@ include('appointment.php');
 <!-- departments section styling -->
 <link href="/css/departments.css" rel="stylesheet">
 
-<style>
-    .infertility {
-    width: 100%;
-    height: auto;
-}
-
-@media (min-width: 768px) {
-    .infertility {
-        width: 80%; 
-        max-width: 700px; 
-    }
-}
-</style>
-
 <body>
     <div class="container mt-3">
         <div class="row align-items-center">
@@ -28,9 +14,7 @@ include('appointment.php');
                 <p class="custom-justify">Infertility is the inability to conceive after a year of regular, unprotected intercourse. It affects both men and women and can be caused by various factors, including hormonal imbalances, reproductive organ issues, or underlying medical conditions. Lifestyle choices, age, and stress can also contribute to infertility. Diagnosis typically involves medical tests and evaluations to determine the root cause. Treatment options range from medication and lifestyle changes to assisted reproductive technologies like IVF. Early intervention can improve the chances of successful conception.</p>
             </div>
             <div class="col-12 col-md-6">
-                <div class="col-12 col-md-6">
-                    <img src="../img/departments/infertility.jpg" class="infertility" alt="Infertility">
-                </div>
+                <img src="../img/departments/infertility.jpg" class="infertility" alt="Infertility">
             </div>
         </div>
 
